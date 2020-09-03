d64c6c62f0312edac651da65a7a53cc24fb89412
thalysonres <thalysonres@gmail.com>
2020-09-03 23:02:18 +0000

    (31/08) Término da configuração da conexão de BD e primeiro model

---
+++ back-end/models/Curso.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const esquema = mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    ementa: {
+        type: String,
+        required: true
+    },
+    carga_horaria: {
+        type: Number,
+        required: true,
+        min: 4,
+        max: 80
+    },
+    nivel: {
+        type: String,
+        required: true,
+        enum: ['Básico', 'Intermediário', 'Avançado']
+    },
+    valor_curso: {
+        type: Number,
+        required: true,
+        default: 450, // Valor assumido se não for informado
+        min: 50
+    }
+})
+
+/*
+    Parâmetros do método mongoose.model()
+    1º -> Nome do modelo (sempre igual a nome do arquivo)
+    2º -> Estrutura (esquema) do modelo
+    3º -> Nome de coleção (collection) em que os objetos criados a partir do
+    modelo serão armazenados no MongoDB
+*/
+module.exports = mongoose.model('Curso', esquema, 'cursos')
