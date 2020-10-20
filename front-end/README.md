d3ce0184944fd3f94640ab53019a5224499e382f
thalysonres <thalysonres@gmail.com>
2020-10-20 00:42:12 +0000

    (19/10) Reinicio do projeto front-end  com Angular 10

--- front-end/README.md
+++ front-end/README.md
@@ -1,6 +1,6 @@
 # FrontEnd
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.7.
 
 ## Development server
 
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './ui/principal/principal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PrincipalComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
